00beebb28212025656375caf7c8d63f08f84e753
Craig Nielsen <craiglbn@gmail.com>
2017-05-05 08:10:12 +0200

    some added stuff

--- magicRename.py
+++ magicRename.py
@@ -6,9 +6,9 @@ if __name__ == '__main__':
         f = open(directory + fname, 'r')
         lines = f.readlines()
         f.close()
-        
+
         f2 = open(directory + fname, 'w')
         for l in lines:
             f2.write(l.replace('__BEGIN__', '__CV_BEGIN__').replace('__END__', '__CV_END__').replace('EXIT', 'exit(0)'))
         f2.close()
-    
+
